f9973cd6c300d5d1c496d588855c0a653f0166a2
Your Name <you@example.com>
2019-10-15 21:15:41 +0300

    modified version

--- src/lab3/Text.java
+++ src/lab3/Text.java
@@ -71,20 +71,20 @@ public class Text {
         String[] words=str.split(" ");  //Split the word from String
         int wrc=1;    //Variable for getting Repeated word count
 
-        for(int i=0;i<words.length;i++) //Outer loop for Comparison
+        for(int i=0;i<words.length;i++)
         {
-            for(int j=i+1;j<words.length;j++) //Inner loop for Comparison
+            for(int j=i+1;j<words.length;j++)
             {
 
-                if(words[i].equals(words[j]))  //Checking for both strings are equal
+                if(words[i].equals(words[j]))  //Checking if both strings are equal
                 {
-                    wrc=wrc+1;    //if equal increment the count
+                    wrc=wrc+1;
                     words[j]="0"; //Replace repeated words by zero
                 }
             }
             if(words[i]!="0"  && wrc >2)
-                System.out.println(words[i]+"--"+wrc); //Printing the word along with count
-            wrc=1;
+                System.out.println(words[i]+"--"+wrc);
+                wrc=1;
 
         }
     }
